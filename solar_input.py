4c959e3f55b251c8a911f9c17e5316a1e9180b48
Leonid Pilyugin <l.pilyugin04@gmail.com>
2022-11-15 10:56:09 +0300

    done

--- solar_input.py
+++ solar_input.py
@@ -18,7 +18,8 @@ def read_space_objects_data_from_file(input_filename):
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
-            object_type = line.split()[0].lower()
+            line = line.lower()
+            object_type = line.split()[0]
             if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
@@ -50,13 +51,13 @@ def parse_star_parameters(line, star):
 
     star_parameters = [str(i) for i in line.split()]
     star.type = star_parameters[0]
-    star.R = int(star_parameters[1])
+    star.R = int(float(star_parameters[1]))
     star.color = star_parameters[2]
-    star.m = int(star_parameters[3])
-    star.x = int(star_parameters[4])
-    star.y = int(star_parameters[5])
-    star.Vx = int(star_parameters[6])
-    star.Vy = int(star_parameters[7])
+    star.m = int(float(star_parameters[3]))
+    star.x = int(float(star_parameters[4]))
+    star.y = int(float(star_parameters[5]))
+    star.Vx = int(float(star_parameters[6]))
+    star.Vy = int(float(star_parameters[7]))
 
 
 def parse_planet_parameters(line, planet):
@@ -76,13 +77,13 @@ def parse_planet_parameters(line, planet):
     """
     planet_parameters = [str(i) for i in line.split()]
     planet.type = planet_parameters[0]
-    planet.R = int(planet_parameters[1])
+    planet.R = int(float(planet_parameters[1]))
     planet.color = planet_parameters[2]
-    planet.m = int(planet_parameters[3])
-    planet.x = int(planet_parameters[4])
-    planet.y = int(planet_parameters[5])
-    planet.Vx = int(planet_parameters[6])
-    planet.Vy = int(planet_parameters[7])
+    planet.m = int(float(planet_parameters[3]))
+    planet.x = int(float(planet_parameters[4]))
+    planet.y = int(float(planet_parameters[5]))
+    planet.Vx = int(float(planet_parameters[6]))
+    planet.Vy = int(float(planet_parameters[7]))
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -98,9 +99,9 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(obj.type + str(obj.R) +
-                  obj.color + str(obj.m) + str(obj.x) +
-                  str(obj.y) + str(obj.Vx) + str(obj.Vy),
+            print(" ".join([obj.type, str(obj.R),
+                  obj.color, str(obj.m), str(obj.x),
+                  str(obj.y), str(obj.Vx), str(obj.Vy)]),
                   end="\n", file=out_file)
 
 
